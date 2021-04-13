b4b4deb6c80bff684c8fdf5e0a2db4afee2aeb69
Jasmeet Singh <jasmeet0915@gmail.com>
2021-04-13 16:46:50 +0530

    makes basic workon_ros function to source required verison setup

--- workon_ros_wrapper.sh
+++ workon_ros_wrapper.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
 
-function workon_ros{
-	ls /opt/share/ros
+function workon_ros {
+	typeset distro_name=$1
+	setup="$WORKON_ROS_HOME/$distro_name/setup.bash"
+
+	if [ ! -f "$setup" ]
+	then
+		echo "ERROR: Environment '$WORKON_ROS_HOME/$env_name' does not contain a setup script." >&2
+		return 1
+	fi
+	
+	source $setup
+	echo "Sourced the setup for ROS $distro_name"
+	
+	PS1="(`basename \"$ROS_DISTRO\"`) ${PS1-}"
 }
